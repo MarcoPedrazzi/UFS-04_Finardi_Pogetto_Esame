68e9689feca748ad5ff1169d772b76cc604105d5
MarcoPedrazzi <marco.pedrazzi.98@gmail.com>
2022-06-24 14:03:06 +0200

    inizializzazione

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='progetto d'esame sulla anali dei dati del dataset AgeDataset-V1.csv',
+    author='Team 2',
+    license='',
+)
